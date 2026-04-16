0dc1b9ade1810b7e39923c77050bfaf485f0c256
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2026-04-16 21:01:04 +0700

    .

--- resources/views/backend/mobile_role2/layout.blade.php
+++ resources/views/backend/mobile_role2/layout.blade.php
@@ -337,6 +337,14 @@
             color: #fff;
         }
 
+        .bottom-nav a span {
+            display: none;
+        }
+
+        .bottom-nav a.active span {
+            display: block;
+        }
+
         .empty-state {
             padding: 20px 16px;
             text-align: center;
