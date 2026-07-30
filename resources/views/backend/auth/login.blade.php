431a267c89459394b93f74b4935ee9abe2e079b7
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2026-07-30 23:12:20 +0700

    .

--- resources/views/backend/auth/login.blade.php
+++ resources/views/backend/auth/login.blade.php
@@ -565,7 +565,7 @@
                     </form>
 
                     <p class="login-note">
-                         Tampilan login telah dioptimalkan agar tetap nyaman digunakan pada layar besar maupun layar kecil.
+                         segsrg Tampilan login telah dioptimalkan agar tetap nyaman digunakan pada layar besar maupun layar kecil.
                     </p>
                 </div>
             </section>
